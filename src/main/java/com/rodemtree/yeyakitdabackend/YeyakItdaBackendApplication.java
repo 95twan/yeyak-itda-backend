2c644a216819db79f9e71090cf3ef1490234c5b6
김태완 <95twan@naver.com>
2025-07-26 22:41:44 +0900

    project init

---
+++ src/main/java/com/rodemtree/yeyakitdabackend/YeyakItdaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rodemtree.yeyakitdabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyakItdaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeyakItdaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodemtree/yeyakitdabackend/YeyakItdaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodemtree.yeyakitdabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeyakItdaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
