3e07330fcf0e6d98f5694aea97fa6ecd153838c1
김태완 <95twan@naver.com>
2025-08-09 23:28:59 +0900

    SignUp Service 테스트 실패 케이스 작성
    실패 - 이미 가입된 이메일로 회원가입을 시도한면 예외를 발생시킨다.
    
    ref #5

--- src/test/java/com/rodemtree/yeyakitda/service/UserServiceTest.java
+++ src/test/java/com/rodemtree/yeyakitda/service/UserServiceTest.java
@@ -2,7 +2,9 @@ package com.rodemtree.yeyakitda.service;
 
 import com.rodemtree.yeyakitda.dto.request.SignUpRequestDto;
 import com.rodemtree.yeyakitda.entity.UserEntity;
+import com.rodemtree.yeyakitda.exception.DuplicateException;
 import com.rodemtree.yeyakitda.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,12 +14,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 
 @DisplayName("비즈니스 로직 - 유저")
 @ExtendWith(MockitoExtension.class)
@@ -36,16 +38,17 @@ class UserServiceTest {
     @DisplayName("정상 - 회원가입에 필요한 정보가 모두 주어지면 새로운 사용자를 생성해 저장한다.")
     void signUpTest() {
         // Given
-        SignUpRequestDto dto = mockSignUpRequestDto();
+        SignUpRequestDto dto = createSignUpRequestDto();
         String expectedEncodedPassword = "encoded" + dto.password();
-        given(userRepository.findByEmail(dto.email())).willReturn(Optional.empty());
+        given(userRepository.existsByEmail(dto.email())).willReturn(false);
+        given(userRepository.existsByNickname(dto.nickname())).willReturn(false);
+        given(userRepository.existsByPhoneNumber(dto.phoneNumber())).willReturn(false);
         given(passwordEncoder.encode(dto.password())).willReturn(expectedEncodedPassword);
 
         // When
         userService.signUp(dto);
 
         // Then
-        then(userRepository).should().findByEmail(dto.email());
         then(passwordEncoder).should().encode(dto.password());
 
         ArgumentCaptor<UserEntity> userCaptor = ArgumentCaptor.forClass(UserEntity.class);
@@ -60,6 +63,56 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("실패 - 이미 가입된 이메일로 회원가입을 시도한면 예외를 발생시킨다.")
+    void signUpWithDuplicateEmailTest() {
+        // Given
+        SignUpRequestDto dto = createSignUpRequestDto();
+        given(userRepository.existsByEmail(dto.email())).willReturn(true);
+
+        // When & Then
+        assertThatThrownBy(() -> userService.signUp(dto))
+                .isInstanceOf(DuplicateException.class)
+                .hasMessageContaining("이메일");
+
+        then(userRepository).should().existsByEmail(dto.email());
+        then(userRepository).should(never()).save(any());
+    }
+
+    @Test
+    @DisplayName("실패 - 이미 가입된 닉네임으로 회원가입을 시도한면 예외를 발생시킨다.")
+    void signUpWithDuplicateNicknameTest() {
+        // Given
+        SignUpRequestDto dto = createSignUpRequestDto();
+        given(userRepository.existsByNickname(dto.nickname())).willReturn(true);
+
+        // When & Then
+        assertThatThrownBy(() -> userService.signUp(dto))
+                .isInstanceOf(DuplicateException.class)
+                .hasMessageContaining("닉네임");
+
+        then(userRepository).should().existsByNickname(dto.nickname());
+        then(userRepository).should(never()).save(any());
+    }
+
+    @Test
+    @DisplayName("실패 - 이미 가입된 전화번호로 회원가입을 시도한면 예외를 발생시킨다.")
+    void signUpWithDuplicatePhoneNumberTest() {
+        // Given
+        SignUpRequestDto dto = createSignUpRequestDto();
+        given(userRepository.existsByPhoneNumber(dto.phoneNumber())).willReturn(true);
+
+        // When & Then
+        assertThatThrownBy(() -> userService.signUp(dto))
+                .isInstanceOf(DuplicateException.class)
+                .hasMessageContaining("전화번호");
+
+        then(userRepository).should().existsByPhoneNumber(dto.phoneNumber());
+        then(userRepository).should(never()).save(any());
+    }
+
+
+    @Disabled
+    @Test
     @DisplayName("정상 - 존재하는 이메일과 비밀번호가 주어졌을 때 access-token을 발급한다.")
     void loginTest() {
         // Given
@@ -73,7 +126,7 @@ class UserServiceTest {
 
     }
 
-    private SignUpRequestDto mockSignUpRequestDto() {
-        return SignUpRequestDto.of("김태완", "test@test.com", "test1234!", "rodem", "경기도 구리시");
+    private SignUpRequestDto createSignUpRequestDto() {
+        return SignUpRequestDto.of("김태완", "test@test.com", "test1234!", "rodem", "경기도 구리시", "010-1234-5678");
     }
 }
