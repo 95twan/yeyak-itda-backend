c7171cc07122aeca107b4c363f843e0cd47a5c54
김태완 <95twan@naver.com>
2025-09-01 19:29:50 +0900

    get-my-user-info api 추가
    ref #55

--- src/main/java/com/rodemtree/yeyakitda/controller/UserController.java
+++ src/main/java/com/rodemtree/yeyakitda/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.rodemtree.yeyakitda.controller;
 
-import com.rodemtree.yeyakitda.dto.request.SignUpRequestDto;
+import com.rodemtree.yeyakitda.dto.UserInfoDto;
 import com.rodemtree.yeyakitda.dto.response.ApiResponseDto;
 import com.rodemtree.yeyakitda.dto.response.ResponseSuccessCode;
+import com.rodemtree.yeyakitda.security.CustomUserDetails;
 import com.rodemtree.yeyakitda.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +20,14 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponseDto<UserInfoDto>> getMyInfo(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        String userEmail = userDetails.getUsername();
+        UserInfoDto userInfoDto = userService.getUserInfo(userEmail);
+        ApiResponseDto<UserInfoDto> responseDto = ApiResponseDto.of(ResponseSuccessCode.USER_INFO, userInfoDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
 
 }
--- src/main/java/com/rodemtree/yeyakitda/service/UserService.java
+++ src/main/java/com/rodemtree/yeyakitda/service/UserService.java
@@ -1,10 +1,12 @@
 package com.rodemtree.yeyakitda.service;
 
+import com.rodemtree.yeyakitda.dto.UserInfoDto;
 import com.rodemtree.yeyakitda.dto.request.SignUpRequestDto;
 import com.rodemtree.yeyakitda.entity.UserEntity;
 import com.rodemtree.yeyakitda.exception.DuplicateException;
 import com.rodemtree.yeyakitda.mapper.UserMapper;
 import com.rodemtree.yeyakitda.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,11 +20,10 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     public void signUp(SignUpRequestDto dto) {
         validateSignUpRequestDto(dto);
-
-        UserMapper userMapper = UserMapper.INSTANCE;
 
         String encodedPassword = passwordEncoder.encode(dto.password());
 
@@ -39,5 +40,11 @@ public class UserService {
         if(userRepository.existsByPhoneNumber(dto.phoneNumber())) duplicatedFields.add(DuplicateException.Field.PHONE_NUMBER);
 
         if(!duplicatedFields.isEmpty()) throw new DuplicateException(duplicatedFields);
+    }
+
+    public UserInfoDto getUserInfo(String userEmail) {
+        UserEntity userEntity = userRepository.findByEmail(userEmail).orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return userMapper.userEntityToUserInfoDto(userEntity);
     }
 }
