8e7c270a4bab5730ff080fd8ca520bf59301c4a0
김태완 <95twan@naver.com>
2025-09-04 17:03:20 +0900

    create review 기능 추가
    ref #64

--- src/test/java/com/rodemtree/yeyakitda/service/ReviewServiceTest.java
+++ src/test/java/com/rodemtree/yeyakitda/service/ReviewServiceTest.java
@@ -2,26 +2,33 @@ package com.rodemtree.yeyakitda.service;
 
 
 import com.rodemtree.yeyakitda.dto.ReviewDto;
+import com.rodemtree.yeyakitda.dto.request.ReviewRequestDto;
+import com.rodemtree.yeyakitda.entity.RestaurantEntity;
 import com.rodemtree.yeyakitda.entity.ReviewEntity;
 import com.rodemtree.yeyakitda.entity.ReviewImageEntity;
+import com.rodemtree.yeyakitda.entity.UserEntity;
 import com.rodemtree.yeyakitda.mapper.ReviewMapper;
+import com.rodemtree.yeyakitda.repository.RestaurantRepository;
 import com.rodemtree.yeyakitda.repository.ReviewImageRepository;
 import com.rodemtree.yeyakitda.repository.ReviewRepository;
+import com.rodemtree.yeyakitda.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Limit;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 
 @DisplayName("비즈니스 로직 - 리뷰")
 @ExtendWith(MockitoExtension.class)
@@ -35,11 +42,17 @@ class ReviewServiceTest {
     @Mock
     private ReviewImageRepository reviewImageRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RestaurantRepository restaurantRepository;
+
     private final ReviewMapper reviewMapper = Mappers.getMapper(ReviewMapper.class);
 
     @BeforeEach
     public void setUp() {
-        reviewService = new ReviewService(reviewRepository, reviewImageRepository, reviewMapper);
+        reviewService = new ReviewService(reviewRepository, reviewImageRepository, userRepository, restaurantRepository, reviewMapper);
     }
 
     @Test
@@ -97,6 +110,36 @@ class ReviewServiceTest {
         then(reviewImageRepository).should().findByReview_Id(reviewEntity2.getId());
     }
 
+    @Test
+    @DisplayName("성공 - 리뷰 정보가 주어지면, 리뷰를 생성하고 저장한다.")
+    void createReviewTest() {
+        // Given
+        Long restaurantId = 1L;
+        String userEmail = "test@test.com";
+        ReviewRequestDto reviewRequestDto = new ReviewRequestDto(4, "test comment");
+        List<MultipartFile> images = List.of();
+
+        UserEntity userEntity = UserEntity.builder().email(userEmail).build();
+        RestaurantEntity restaurantEntity = RestaurantEntity.builder().build();
+
+        given(userRepository.findByEmail(userEmail)).willReturn(Optional.of(userEntity));
+        given(restaurantRepository.findById(restaurantId)).willReturn(Optional.of(restaurantEntity));
+
+        // When
+        reviewService.createReview(restaurantId, userEmail, reviewRequestDto, images);
+
+        // Then
+        ArgumentCaptor<ReviewEntity> reviewCaptor = ArgumentCaptor.forClass(ReviewEntity.class);
+        then(reviewRepository).should().save(reviewCaptor.capture());
+        ReviewEntity reviewEntity = reviewCaptor.getValue();
+
+        assertThat(reviewEntity.getRating()).isEqualTo(reviewRequestDto.rating());
+        assertThat(reviewEntity.getComment()).isEqualTo(reviewRequestDto.comment());
+        assertThat(reviewEntity.getUser()).isEqualTo(userEntity);
+        assertThat(reviewEntity.getRestaurant()).isEqualTo(restaurantEntity);
+
+    }
+
     private ReviewEntity createReviewEntity(String comment, Integer rating) {
         return createReviewEntity(null, comment, rating);
     }
@@ -110,4 +153,5 @@ class ReviewServiceTest {
     private ReviewImageEntity createReviewImage(ReviewEntity review, String imageUrl) {
         return ReviewImageEntity.of(review, imageUrl);
     }
+
 }
